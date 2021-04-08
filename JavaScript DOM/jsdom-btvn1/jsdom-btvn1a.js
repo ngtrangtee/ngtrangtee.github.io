6129a91e66f7865c56a3fd5658e23cc5c6739327
Tee Nguyen <ng.thaotrang05@gmail.com>
2021-04-08 14:12:29 +0700

    jsdom-btvn1

--- JavaScript DOM/jsdom-btvn1/jsdom-btvn1a.js
+++ JavaScript DOM/jsdom-btvn1/jsdom-btvn1a.js
@@ -1,8 +1,9 @@
 // Bài 1: Tạo 1 thẻ p có id="text", có nội dung bất kỳ. Yêu cầu
 //- Đặt màu văn bản thành #777
-document.body.style.backgroundColor = '#777';
+const text = document.getElementById("text");
+text.style.color = '#777';
 //- Đặt kích thước phông chữ thành 2rem
-const text = document.getElementById('text');
+
 text.style.fontSize = '2rem'
 //- Đặt nội dung HTML thành "Tôi có thể làm <em> bất cứ điều gì </em> tôi muốn với JavaScript."
 text.innerHTML = 'Tôi có thể làm <em> bất cứ điều gì </em> tôi muốn với JavaScript.';
